235c514539e6b4649b1b228149ce1acfb1a2424e
DigitalHain <digitalhain@gmail.com>
2021-05-15 22:26:30 -0700

    Initial commit

---
+++ app/src/main/java/com/digitalhain/daipsi/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.digitalhain.daipsi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_SCREEN_TIME_OUT=2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashActivity.this, GetStartedActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
+    }
+}
